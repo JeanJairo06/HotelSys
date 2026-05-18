f1e9462d6bd863b09e47b5dcdfaa886e0aa718b0
jfernandezcarlos-07 <jfernandezcarlo@uss.edu.pe>
2026-05-18 16:50:47 -0500

    doc: se agrego el modelo factura al admin

--- facturacion/admin.py
+++ facturacion/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Folio
+from .models import Folio, Factura
+
+
+class FacturaInline(admin.TabularInline):
+    model = Factura
+    extra = 0
+    readonly_fields = ('monto_subtotal', 'monto_igv', 'monto_total', 'fecha_emision')
+    can_delete = False
 
 
 @admin.register(Folio)
@@ -11,3 +18,17 @@ class FolioAdmin(admin.ModelAdmin):
         'estancia__reserva__huesped__apellidos',
     )
     list_filter = ('estado',)
+    readonly_fields = ('subtotal', 'igv', 'total')
+    inlines = [FacturaInline]
+
+
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'folio', 'ruc_dni', 'razon_social', 'monto_total', 'fecha_emision')
+    search_fields = ('ruc_dni', 'razon_social', 'folio__id')
+    list_filter = ('fecha_emision',)
+    readonly_fields = ('folio', 'ruc_dni', 'razon_social', 'monto_subtotal', 'monto_igv', 'monto_total', 'fecha_emision')
+
+    def has_add_permission(self, request):
+        
+        return False
