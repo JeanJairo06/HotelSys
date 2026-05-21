7b4d72efc451d4576c51d670923835f9bc451b71
Luis Guerrero <luissguerreroo3@gmail.com>
2026-05-21 02:45:43 -0500

    feat: reservas, calendar y detail

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from reservas.views import ReservaCancelView, ReservaCreateView, ReservaListView, ReservaUpdateView
+from reservas.views import (
+    ReservaCalendarView,
+    ReservaCancelView,
+    ReservaCreateView,
+    ReservaDetailView,
+    ReservaListView,
+    ReservaUpdateView,
+)
 
 
 app_name = 'reservas'
@@ -8,6 +15,8 @@ app_name = 'reservas'
 urlpatterns = [
     path('', ReservaListView.as_view(), name='list'),
     path('crear/', ReservaCreateView.as_view(), name='create'),
+    path('calendario/', ReservaCalendarView.as_view(), name='calendar'),
+    path('<int:pk>/', ReservaDetailView.as_view(), name='detail'),
     path('<int:pk>/editar/', ReservaUpdateView.as_view(), name='update'),
     path('<int:pk>/cancelar/', ReservaCancelView.as_view(), name='cancel'),
 ]
