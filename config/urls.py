957f7ec08753174061f9857e9db0e8b2fc6ec4f5
jfernandezcarlos-07 <jfernandezcarlo@uss.edu.pe>
2026-05-21 02:15:59 -0500

    feat: se creo los HTML para folio y tarifa

--- config/urls.py
+++ config/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
+
+    path('facturacion/', include('facturacion.urls')),
 ]
 
 handler403 = 'config.views.error_403'
--- facturacion/urls.py
+++ facturacion/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-from . import views
-
+#from . import views
+from facturacion.views import (
+    TarifaListView, TarifaCreateView, TarifaUpdateView, TarifaDeleteView,
+    FolioListView, FolioDetailView, CargoEstanciaCreateView, FacturaCreateView
+)
 app_name = 'facturacion'
 
 urlpatterns = [
-    path('folios/', views.FolioListView.as_view(), name='folio_list'),
-    path('folios/<int:pk>/', views.FolioDetailView.as_view(), name='folio_detail'),
-    path('folios/<int:folio_id>/facturar/', views.FacturaCreateView.as_view(), name='factura_create'),
+    path('folios/', FolioListView.as_view(), name='folio_list'),
+    path('folios/<int:pk>/', FolioDetailView.as_view(), name='folio_detail'),
+    path('folios/<int:folio_id>/facturar/', FacturaCreateView.as_view(), name='factura_create'),
 
-    path('folios/<int:folio_id>/agregar-cargo/', views.CargoEstanciaCreateView.as_view(), name='cargo_create'),
-    path('tarifas/', views.TarifaListView.as_view(), name='tarifa_list'),
-    path('tarifas/crear/', views.TarifaCreateView.as_view(), name='tarifa_create'),
-    path('tarifas/<int:pk>/editar/', views.TarifaUpdateView.as_view(), name='tarifa_update'),
-    path('tarifas/<int:pk>/eliminar/', views.TarifaDeleteView.as_view(), name='tarifa_delete'),
+    path('folios/<int:folio_id>/agregar-cargo/', CargoEstanciaCreateView.as_view(), name='cargo_create'),
+    path('tarifas/', TarifaListView.as_view(), name='tarifa_list'),
+    path('tarifas/crear/', TarifaCreateView.as_view(), name='tarifa_create'),
+    path('tarifas/<int:pk>/editar/', TarifaUpdateView.as_view(), name='tarifa_update'),
+    path('tarifas/<int:pk>/eliminar/', TarifaDeleteView.as_view(), name='tarifa_delete'),
 ]
