950c0ee7f1701e2a960ada9fce24ca6e2cdea121
DanielEscribanoM28 <emacalopudaniel@uss.edu.pe>
2026-07-17 22:08:02 -0500

    test: ajustar limpieza api

--- api/tests.py
+++ api/tests.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from config.choices import EstadoHabitacion, EstadoReserva
-from cuentas.roles import ROLE_RECEPCIONISTA
+from cuentas.roles import ROLE_HOUSEKEEPING, ROLE_RECEPCIONISTA
 from estancias.services import registrar_checkin
 from habitaciones.models import Habitacion, TipoHabitacion
 from hoteles.models import Hotel
@@ -72,3 +72,74 @@ class EstanciasAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['code'], 'CHECKOUT_BLOQUEADO')
         self.assertEqual(response.data['message'], 'No se puede hacer checkout. Saldo pendiente: S/ 141.60.')
+
+
+class HousekeepingAPITests(TestCase):
+    def setUp(self):
+        grupo, _ = Group.objects.get_or_create(name=ROLE_HOUSEKEEPING)
+        self.usuario = User.objects.create_user(username='limpieza', password='testpass123')
+        self.usuario.groups.add(grupo)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.usuario)
+
+        self.hotel = Hotel.objects.create(
+            nombre='Hotel Central',
+            ruc='12345678901',
+            direccion='Av. Principal 123',
+            estrellas=4,
+            telefono='999999999',
+        )
+        self.tipo = TipoHabitacion.objects.create(
+            nombre='Simple',
+            capacidad=1,
+            precio_base=Decimal('120.00'),
+        )
+        self.habitacion = Habitacion.objects.create(
+            hotel=self.hotel,
+            tipo=self.tipo,
+            numero='201',
+            piso=2,
+            estado=EstadoHabitacion.LIMPIEZA,
+        )
+
+    def test_housekeeping_api_marca_disponible_desde_limpieza(self):
+        response = self.client.patch(
+            f'/api/v1/habitaciones/{self.habitacion.id}/housekeeping/',
+            {'estado': EstadoHabitacion.DISPONIBLE},
+            format='json',
+        )
+
+        self.habitacion.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.habitacion.estado, EstadoHabitacion.DISPONIBLE)
+        self.assertEqual(response.data['estado'], EstadoHabitacion.DISPONIBLE)
+
+    def test_housekeeping_api_bloquea_disponible_si_no_esta_en_limpieza(self):
+        self.habitacion.estado = EstadoHabitacion.OCUPADA
+        self.habitacion.save(update_fields=['estado'])
+
+        response = self.client.patch(
+            f'/api/v1/habitaciones/{self.habitacion.id}/housekeeping/',
+            {'estado': EstadoHabitacion.DISPONIBLE},
+            format='json',
+        )
+
+        self.habitacion.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['code'], 'HOUSEKEEPING_TRANSICION_INVALIDA')
+        self.assertEqual(self.habitacion.estado, EstadoHabitacion.OCUPADA)
+
+    def test_housekeeping_api_envia_disponible_a_mantenimiento(self):
+        self.habitacion.estado = EstadoHabitacion.DISPONIBLE
+        self.habitacion.save(update_fields=['estado'])
+
+        response = self.client.patch(
+            f'/api/v1/habitaciones/{self.habitacion.id}/housekeeping/',
+            {'estado': EstadoHabitacion.MANTENIMIENTO},
+            format='json',
+        )
+
+        self.habitacion.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.habitacion.estado, EstadoHabitacion.MANTENIMIENTO)
+        self.assertEqual(response.data['estado'], EstadoHabitacion.MANTENIMIENTO)
